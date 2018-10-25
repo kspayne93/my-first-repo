9fcf3ffbb95f5626507bc9a36442810ed98faf9d
kspayne93 <kyle@investavante.com>
2018-10-25 17:48:04 -0600

    code edits for console video practice problems

--- console.js
+++ console.js
@@ -1,9 +1,21 @@
-var names = ["James","Dave","Michael"];
-
-function lookingForDave(arr){
-  for(i;i<arr.length;i++){
-    if(i==="Dave"){
-      return "I found him.";
-    }
-  }
-}
+  var arr1 = [15, 17, 22, 4, 3, 35, 11, 12, 38, 8];
+  // Create a function that will average all the even or odd numbers in an array.
+  function averageArray(array, evensOrOdds) {
+    var total = 0;
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (evensOrOdds == "evens") {
+        if (array[i] % 2 == 0) {
+          total += array[i];
+          count++;
+        };
+      } else if (evensOrOdds == "odds") {
+        if (array[i] % 2 != 0) {
+          total += array[i];
+          count++;
+        };
+      };
+    };
+    return total / count
+  };
+  console.log(averageArray(arr1, "odds"));
